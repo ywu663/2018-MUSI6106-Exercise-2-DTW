87d2ea05d3a9cd04c3fb7bf10763f74b51e061b7
Yi Wu <ywu663@gatech.edu>
2018-04-04 21:07:49 -0400

    DTW process done

--- src/Dtw/Dtw.cpp
+++ src/Dtw/Dtw.cpp
@@ -1,30 +1,44 @@
 
 #include "Vector.h"
 #include "Util.h"
-
+#include <algorithm>
 #include "Dtw.h"
 
 CDtw::CDtw( void )
 {
-
+    
 }
 
 CDtw::~CDtw( void )
 {
-
+    
 }
 
 Error_t CDtw::init( int iNumRows, int iNumCols )
 {
-    m_i_NumRows = iNumRows;
+    m_iNumRows = iNumRows;
     m_iNumCols = iNumCols;
-    costMatrix = new float*[m_i_NumRows];
-    for (int i = 0; i < m_i_NumRows; i++) {
+    costMatrix = new float*[m_iNumRows];
+    for (int i = 0; i < m_iNumRows; i++)
+    {
         costMatrix[i] = new float[iNumCols];
-        for (int j = 0; j < m_iNumCols; j++) {
+        for (int j = 0; j < m_iNumCols; j++)
+        {
             costMatrix[i][j] = 0;
         }
     }
+    directionMatrix = new int*[m_iNumRows];
+    for (int i = 0; i < m_iNumRows; i++)
+    {
+        directionMatrix[i] = new int[iNumCols];
+        for (int j = 0; j < m_iNumCols; j++)
+        {
+            directionMatrix[i][j] = 0;
+        }
+    }
+    
+    
+    
     return kNoError;
 }
 
@@ -36,17 +50,62 @@ Error_t CDtw::reset()
 Error_t CDtw::process(float **ppfDistanceMatrix)
 {
     costMatrix[0][0] = ppfDistanceMatrix[0][0];
+    for (int i = 1; i < m_iNumRows; i++)
+    {
+        for (int j = 1; j < m_iNumCols; j++)
+        {
+            float nextMinCost = std::min(costMatrix[i-1][j-1], std::min(costMatrix[i-1][j], costMatrix[i][j-1]));
+            costMatrix[i][j] = nextMinCost + ppfDistanceMatrix[i][j];
+            if (nextMinCost == costMatrix[i-1][j-1])
+                directionMatrix[i][j] = kDiag;
+            if (nextMinCost == costMatrix[i-1][j])
+                directionMatrix[i][j] = kHoriz;
+            if (nextMinCost == costMatrix[i][j-1])
+                directionMatrix[i][j] = kVert;
+        }
+    }
+    
+    int iCurrentX = m_iNumRows - 1;
+    int iCurrentY = m_iNumCols - 1;
+    
+    while ( iCurrentX >= 0 && iCurrentY >= 0)
+    {
+        int iCurrentDirection = directionMatrix[iCurrentX][iCurrentY];
+        if (iCurrentDirection == kVert)
+        {
+            iCurrentY--;
+            m_fPathCost += costMatrix[iCurrentX][iCurrentY];
+        }
+        
+        if (iCurrentDirection == kHoriz)
+        {
+            iCurrentX--;
+            m_fPathCost += costMatrix[iCurrentX][iCurrentY];
+        }
+        if (iCurrentDirection == kDiag){
+            iCurrentY--;
+            iCurrentX--;
+            m_fPathCost += costMatrix[iCurrentX][iCurrentY];
+            
+        }
+        m_iPathLength++;
+    }
+    
+    
+    
+    
+    
     return kNoError;
 }
 
 int CDtw::getPathLength()
-{    
-    return 0;
+{
+    return m_iPathLength;
 }
 
 float CDtw::getPathCost() const
 {
-    return 0.f;
+    return m_fPathCost;
 }
 
 Error_t CDtw::getPath( int **ppiPathResult ) const
--- src/inc/Dtw.h
+++ src/inc/Dtw.h
@@ -7,7 +7,7 @@
 #include "ErrorDef.h"
 
 /*! \brief basic dynamic time warping (http://www.audiocontentanalysis.org/code/helper-functions/dtw/)
-*/
+ */
 class CDtw
 {
 public:
@@ -15,61 +15,65 @@ public:
     {
         kRow,
         kCol,
-
+        
         kNumMatrixDimensions
     };
-
+    
     CDtw(void);
     virtual ~CDtw(void);
-
+    
     /*! initializes the class with the size of the distance matrix
-    \param iNumRows
-    \param iNumCols
-    \return Error_t
-    */
+     \param iNumRows
+     \param iNumCols
+     \return Error_t
+     */
     Error_t init (int iNumRows, int iNumCols);
     
     /*! resets all internal class members
-    \return Error_t
-    */
+     \return Error_t
+     */
     Error_t reset ();
-
+    
     /*! computes cost and path w/o back-tracking
-    \param ppfDistanceMatrix (dimensions [rows][columns])
-    \return Error_t
-    */
+     \param ppfDistanceMatrix (dimensions [rows][columns])
+     \return Error_t
+     */
     Error_t process (float **ppfDistanceMatrix);
- 
+    
     /*! returns the length of the path
-    \return int
-    */
+     \return int
+     */
     int getPathLength ();
     
     /*! returns the overall cost
-    \return float
-    */
+     \return float
+     */
     float getPathCost () const;
     
     /*! returns the path \sa getPathLength
-    \param ppiPathResult pointer to memory the result is written to (dimensions [2][length_of_path])
-    \return Error_t
-    */
+     \param ppiPathResult pointer to memory the result is written to (dimensions [2][length_of_path])
+     \return Error_t
+     */
     Error_t getPath (int **ppiPathResult) const;
-
+    
 private:
     enum Directions_t
     {
         kHoriz,
         kVert,
         kDiag,
-
+        
         kNumDirections
     };
     
     float** costMatrix = 0;
-    int m_i_NumRows;
-    int m_iNumCols;
+    int ** directionMatrix = 0;
+    int m_iNumRows = 0;
+    int m_iNumCols = 0;
+    int m_iPathLength = 0;
+    float m_fPathCost = 0;
 };
 
 
 #endif
+
